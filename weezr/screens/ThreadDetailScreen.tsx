be4fab5bba69349babf9b23ce76ee25507714af2
PierreMartin <pierremartin.pro@gmail.com>
2022-12-08 11:37:15 +0100

    renew buttons actions chat

--- weezr/screens/ThreadDetailScreen.tsx
+++ weezr/screens/ThreadDetailScreen.tsx
@@ -980,9 +980,153 @@ function ThreadDetailScreenComponent({
         return null;
     };
 
+    const onSetIsChatActionsOpened = (isOpen: boolean) => {
+        if (isOpen) { setIsChatActionsOpened(isOpen); }
+
+        offsetChatActionsShared.value = withTiming(isOpen ? 0 : offsetChatActions, { duration: 500, easing: Easing.out(Easing.exp) }, (finished) => {
+            if (finished && !isOpen) { setIsChatActionsOpened(isOpen); }
+        });
+
+        offsetComposerShared.value = withTiming(isOpen ? 0 : offsetComposer, { duration: 500, easing: Easing.out(Easing.exp) });
+    };
+
+    /* Handling animations: */
     const offsetChatActionsStyles = useAnimatedStyle(() => ({ transform: [{ translateX: offsetChatActionsShared.value }] }));
     // const offsetComposerStyles = useAnimatedStyle(() => ({ transform: [{ translateX: offsetComposerShared.value }] }));
     const offsetComposerStyles = useAnimatedStyle(() => ({ width: `${offsetComposerShared.value * 100}%` }));
+
+    const renderActions = (/* props: Readonly<ActionsProps> */) => {
+        return (
+            <Box style={styles.actionsContainer}>
+                <Animated.View style={[{ position: 'absolute', bottom: 8, left: 0 }, offsetChatActionsStyles]}>
+                    <Box style={{ flex: 1, flexDirection: 'row' }}>
+                        <Box style={[styles.actionsItem]}>
+                            {/* Open my images picker */}
+                            <Button
+                                rounded="none"
+                                variant="unstyle"
+                                pl="0"
+                                pr="0"
+                                _text={{ fontSize: 16 }}
+                                onPress={() => {
+                                    // Use => https://gorhom.github.io/react-native-bottom-sheet/usage
+                                    onOpenMyPhotosPicker();
+                                }}
+                            >
+                                📷
+                            </Button>
+                        </Box>
+
+                        <Box style={[styles.actionsItem, { marginHorizontal: 0 }]}>
+                            <Button
+                                rounded="none"
+                                variant="unstyle"
+                                pl="0"
+                                pr="0"
+                                _text={{ fontSize: 16 }}
+                                onPress={() => {
+                                    if (showActionSheetWithOptions) {
+                                        const options = [
+                                            'Close',
+                                            'Send my position'
+                                        ];
+
+                                        const hasAlreadySentRequest = !!thread.userInteractions?.myRequest?.sent?.id;
+                                        const participantsFront = thread?.participants?.filter((participant: IUser) => (participant?.id && (participant.id !== me._id)));
+                                        const hasParticipantFrontPrivatePhotos = participantsFront?.length && participantsFront[0].hasPrivatePhotos;
+
+                                        if (isBetweenTwoUsers && hasParticipantFrontPrivatePhotos && !hasAlreadySentRequest) {
+                                            options.push('Ask private photos');
+                                        }
+
+                                        showActionSheetWithOptions(
+                                            {
+                                                options,
+                                                cancelButtonIndex: 0,
+                                                userInterfaceStyle: 'dark'
+                                            },
+                                            (buttonIndex) => {
+                                                switch (buttonIndex) {
+                                                    case 0:
+                                                        // Close
+                                                        break;
+                                                    case 1:
+                                                        // Send my position
+                                                        onOpenMap(me.currentLocation, true);
+                                                        break;
+                                                    case 2:
+                                                        // Ask private photos
+                                                        if (isBetweenTwoUsers && !hasAlreadySentRequest) {
+                                                            onSendRequest({
+                                                                sendRequest,
+                                                                participantsIds: participantsIdsAll,
+                                                                userFront: { id: participantsIdsFront[0] },
+                                                                me,
+                                                                onSetStateAtSubmit: (userInterSendRequest: IUserInteraction) => {
+                                                                    const nextMessage: IThreadMessage = {
+                                                                        _id: getUniqueId(),
+                                                                        threadId: thread.id,
+                                                                        createdAt: new Date(),
+                                                                        requestId: userInterSendRequest.id,
+                                                                        request: userInterSendRequest,
+                                                                        user: me
+                                                                    };
+
+                                                                    onSetStateNewSuccessMessage(false, nextMessage);
+
+                                                                    setThread((previousThread: IThread) => {
+                                                                        const nextThread = _.cloneDeep(previousThread || {});
+
+                                                                        if (me?._id) {
+                                                                            // eslint-disable-next-line max-len
+                                                                            if (!nextThread?.userInteractions) { nextThread.userInteractions = {} as any; }
+                                                                            // eslint-disable-next-line max-len
+                                                                            if (!nextThread?.userInteractions?.myRequest) { nextThread.userInteractions.myRequest = {} as any; }
+                                                                            // eslint-disable-next-line max-len
+                                                                            if (!nextThread?.userInteractions?.myRequest?.sent) { nextThread.userInteractions.myRequest.sent = {} as any; }
+
+                                                                            nextThread.userInteractions.myRequest.sent = userInterSendRequest;
+                                                                        }
+
+                                                                        return nextThread;
+                                                                    });
+                                                                },
+                                                                socketEvents
+                                                            });
+                                                        }
+                                                        break;
+                                                    default:
+                                                        break;
+                                                }
+                                            }
+                                        );
+                                    }
+                                }}
+                            >
+                                ➕
+                            </Button>
+                        </Box>
+                    </Box>
+                </Animated.View>
+
+                {
+                    !isChatActionsOpened && (
+                        <Box style={{ position: 'absolute', bottom: 8, left: 0 }}>
+                            <Button
+                                leftIcon={<Icon as={Ionicons} name="chevron-forward-outline" size="xl" />}
+                                rounded="none"
+                                variant="unstyle"
+                                p={0}
+                                m={0}
+                                bottom={2}
+                                onPress={() => { onSetIsChatActionsOpened(true); }}
+                            />
+                        </Box>
+                    )
+                }
+            </Box>
+        );
+    };
 
     const renderComposer = (props: any) => {
         // If audio voice recorded:
@@ -1126,149 +1270,6 @@ function ThreadDetailScreenComponent({
             >
                 {send}
             </Send>
-        );
-    };
-
-    const onSetIsChatActionsOpened = (isOpen: boolean) => {
-        if (isOpen) { setIsChatActionsOpened(isOpen); }
-
-        offsetChatActionsShared.value = withTiming(isOpen ? 0 : offsetChatActions, { duration: 500, easing: Easing.out(Easing.exp) }, (finished) => {
-            if (finished && !isOpen) { setIsChatActionsOpened(isOpen); }
-        });
-
-        offsetComposerShared.value = withTiming(isOpen ? 0 : offsetComposer, { duration: 500, easing: Easing.out(Easing.exp) });
-    };
-
-    const renderActions = (/* props: Readonly<ActionsProps> */) => {
-        return (
-            <Box style={styles.actionsContainer}>
-                <Animated.View style={[{ position: 'absolute', bottom: 8, left: 0 }, offsetChatActionsStyles]}>
-                    <Box style={{ flex: 1, flexDirection: 'row' }}>
-                        <Box style={[styles.actionsItem]}>
-                            {/* Open my images picker */}
-                            <Button
-                                rounded="none"
-                                variant="unstyle"
-                                pl="0"
-                                pr="0"
-                                _text={{ fontSize: 16 }}
-                                onPress={() => {
-                                    // Use => https://gorhom.github.io/react-native-bottom-sheet/usage
-                                    onOpenMyPhotosPicker();
-                                }}
-                            >
-                                📷
-                            </Button>
-                        </Box>
-
-                        <Box style={[styles.actionsItem, { marginHorizontal: 0 }]}>
-                            <Button
-                                rounded="none"
-                                variant="unstyle"
-                                pl="0"
-                                pr="0"
-                                _text={{ fontSize: 16 }}
-                                onPress={() => {
-                                    if (showActionSheetWithOptions) {
-                                        const options = [
-                                            'Close',
-                                            'Send my position'
-                                        ];
-
-                                        const hasAlreadySentRequest = !!thread.userInteractions?.myRequest?.sent?.id;
-                                        const participantsFront = thread?.participants?.filter((participant: IUser) => (participant?.id && (participant.id !== me._id)));
-                                        const hasParticipantFrontPrivatePhotos = participantsFront?.length && participantsFront[0].hasPrivatePhotos;
-
-                                        if (isBetweenTwoUsers && hasParticipantFrontPrivatePhotos && !hasAlreadySentRequest) {
-                                            options.push('Ask private photos');
-                                        }
-
-                                        showActionSheetWithOptions(
-                                            {
-                                                options,
-                                                cancelButtonIndex: 0,
-                                                userInterfaceStyle: 'dark'
-                                            },
-                                            (buttonIndex) => {
-                                                switch (buttonIndex) {
-                                                    case 0:
-                                                        // Close
-                                                        break;
-                                                    case 1:
-                                                        // Send my position
-                                                        onOpenMap(me.currentLocation, true);
-                                                        break;
-                                                    case 2:
-                                                        // Ask private photos
-                                                        if (isBetweenTwoUsers && !hasAlreadySentRequest) {
-                                                            onSendRequest({
-                                                                sendRequest,
-                                                                participantsIds: participantsIdsAll,
-                                                                userFront: { id: participantsIdsFront[0] },
-                                                                me,
-                                                                onSetStateAtSubmit: (userInterSendRequest: IUserInteraction) => {
-                                                                    const nextMessage: IThreadMessage = {
-                                                                        _id: getUniqueId(),
-                                                                        threadId: thread.id,
-                                                                        createdAt: new Date(),
-                                                                        requestId: userInterSendRequest.id,
-                                                                        request: userInterSendRequest,
-                                                                        user: me
-                                                                    };
-
-                                                                    onSetStateNewSuccessMessage(false, nextMessage);
-
-                                                                    setThread((previousThread: IThread) => {
-                                                                        const nextThread = _.cloneDeep(previousThread || {});
-
-                                                                        if (me?._id) {
-                                                                            // eslint-disable-next-line max-len
-                                                                            if (!nextThread?.userInteractions) { nextThread.userInteractions = {} as any; }
-                                                                            // eslint-disable-next-line max-len
-                                                                            if (!nextThread?.userInteractions?.myRequest) { nextThread.userInteractions.myRequest = {} as any; }
-                                                                            // eslint-disable-next-line max-len
-                                                                            if (!nextThread?.userInteractions?.myRequest?.sent) { nextThread.userInteractions.myRequest.sent = {} as any; }
-
-                                                                            nextThread.userInteractions.myRequest.sent = userInterSendRequest;
-                                                                        }
-
-                                                                        return nextThread;
-                                                                    });
-                                                                },
-                                                                socketEvents
-                                                            });
-                                                        }
-                                                        break;
-                                                    default:
-                                                        break;
-                                                }
-                                            }
-                                        );
-                                    }
-                                }}
-                            >
-                                ➕
-                            </Button>
-                        </Box>
-                    </Box>
-                </Animated.View>
-
-                {
-                    !isChatActionsOpened && (
-                        <Box style={{ position: 'absolute', bottom: 8, left: 0 }}>
-                            <Button
-                                leftIcon={<Icon as={Ionicons} name="chevron-forward-outline" size="xl" />}
-                                rounded="none"
-                                variant="unstyle"
-                                p={0}
-                                m={0}
-                                bottom={2}
-                                onPress={() => { onSetIsChatActionsOpened(true); }}
-                            />
-                        </Box>
-                    )
-                }
-            </Box>
         );
     };
 
